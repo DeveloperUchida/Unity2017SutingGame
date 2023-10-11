544edc61334f2313031a699f03360e19982a377c
DeveloperUchida <kouei28poiou@icloud.com>
2023-10-11 12:30:59 +0900

    DataFirstUpload 2023/10/11

---
+++ Assets/Scripts/PlayerScripts.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScripts : MonoBehaviour {
+
+	private float PlayerSpped = 0.06f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.W)) {
+			transform.Translate (0, 0, PlayerSpped);
+		}
+		if(Input.GetKey(KeyCode.A)){
+			transform.Translate (-PlayerSpped, 0, 0);
+			}
+		if (Input.GetKey (KeyCode.S)) {
+			transform.Translate (0, 0, -PlayerSpped);
+		}
+
+	}
+}
